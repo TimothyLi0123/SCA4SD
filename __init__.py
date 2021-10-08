f231153c75482800ba76d4643dbccc7975175d09
TimothyLi0123 <59607582+TimothyLi0123@users.noreply.github.com>
2021-10-08 16:23:51 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Tianyi Li'
 __email__ = 'tianyi.li@cuhk.edu.hk'
-__version__ = '1.0.0 - 20211005'
+__version__ = '1.0.2 - 20211008'
 __reference__ = 'Li, T. & Oliva, R. (2021), Structural Control Analysis of System Dynamics Models.'
 
